2c8c69c325df649262f007cf01aaaa45bf46abab
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-12-06 16:02:11 -0800

    changed after confirmation path

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
-    redirect_to confirmed_path
+    redirect_to login_path
+    flash[:notice] = 'Thanks for confirming your email!'
   end
 end
