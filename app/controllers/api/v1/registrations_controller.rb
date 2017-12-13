3829e45f79c6debe2eaa759a396151dabf0397d1
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-12-12 16:00:22 -0800

    skip verify auth token on post actions

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
 
+  skip_before_action :verify_authenticity_token
+
     def create
       @user = User.create(user_params)
       if @user.save
