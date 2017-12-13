7b037ef4fe08e3b810477bbe49d3607b83eba8ff
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-12-12 16:32:28 -0800

    added json_request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def json_request?
+    request.format.json?
+  end
+
   def after_sign_in_path_for(resource)
       if resource.sign_in_count == 1
          add_city_path
