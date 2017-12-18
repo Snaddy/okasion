e84c7dded120b5646fddd57535b487d6dbedfbc1
Hayden Conlin <hconlinmouat@hotmail.comgit config --global user.email hconlinmouat@hotmail.com>
2017-12-18 15:07:08 -0800

    changed action name

--- app/controllers/api/v1/miscs_controller.rb
+++ app/controllers/api/v1/miscs_controller.rb
@@ -12,13 +12,11 @@ class Api::V1::MiscsController < ActionController::Base
 		end
 	end
 
-	def password_reset
+	def reset_password
 		@user = User.find_by(email: params[:email])
 		if User.exists?(email: params[:email])
 			@user.send_reset_password_instructions
 			render json: {status: 'success'}
-		else
-			render json: { status: 'Email not found'}
 		end
 	end
 
